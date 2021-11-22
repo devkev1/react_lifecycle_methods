f48fdad0a62a4e9c5b1ca64ac28c424fbb421662
Kevin Wong <kevingywong@gmail.com>
2021-11-22 12:52:17 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,61 @@
 import React, { Component } from "react";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      hasLoaded: false,
+      title: "React App",
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleLoadedChange = this.handleLoadedChange.bind(this);
+    this.displayLoading = this.displayLoading.bind(this);
+  }
+
+  handleInput() {
+    this.setState({
+      title: document.getElementById("title").value,
+    });
+  }
+
+  handleLoadedChange() {
+    this.setState({
+      hasLoaded: this.state.hasLoaded ? false : true,
+    });
+  }
+
+  displayLoading() {
+    if (this.state.hasLoaded) {
+      return ( 
+        <div>
+          <input 
+          type="text" 
+          name="title" 
+          id="title" 
+          placeholder="Enter"
+          />
+          <button onClick={this.handleInput}>Change Title</button>
+        </div>
+      );
+    } else {
+    return <h3>Loading...</h3>;
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      hasLoaded: true,
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>Welcome to React!</h1>
-        <hr />
+        <h1>{this.state.title}</h1>
+        <button onClick={this.handleLoadedChange}>Toggle Loaded Change</button>
+        <hr />     
+      {this.displayLoading()}
       </div>
     );
   }
